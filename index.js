5209b69e03633a68ec8527c09f35ea0ac354535b
chilaganisriram <chilaganisriram07@gmail.com>
2025-03-06 02:31:46 +0530

    06-3-25 array basics

--- index.js
+++ index.js
@@ -168,3 +168,50 @@ calculate(10,5,(a,b)=>{if (a>b){ return (a+b)*3} if (a<b){ return (b-a)*3} else{
             console.log(i);
         }
     }
+
+    //06-03-25
+
+//     1. Create an Array
+//  Create an array with five numbers and print it.
+
+var arr = [1,2,3,4,5]
+console.log(arr);
+// 2. Access an Element
+//  Given an array, print the third element
+console.log(arr[2]);
+// 3. Change an Element
+//  Modify the second element of an array to a new value
+arr[1]=9
+// 4. Find Length
+//  Write a function that returns the length of an array
+console.log(arr.length);
+// 5. Add an Element (Push)
+// Add a new element to the end of an array
+arr.push(8)
+console.log(arr);
+// 6. Remove the Last Element (Pop)
+// Remove the last element from an array and print the updated array
+arr.pop()
+console.log(arr);
+// 7. Loop Through an Array
+// Use a loop to print each element of an array
+for( var element of arr){
+    console.log(element);
+}
+// 8. Check if an Element Exists
+// Write a function to check if a given value exists in an array.
+function elementExists(arr, value) {
+    return arr.includes(value);
+  }
+  elementExists(arr,3)
+//   9. Copy an Array
+//   Create a copy of an array and print it
+var copiedArray = [];
+for (var i = 0; i < arr.length; i++){
+  copiedArray.push(arr[i]);
+}
+// 10. Convert Array to String
+//  Convert an array to a string using .join()
+// .join() and print the result
+let arrayString = arr.join(",");
+  console.log(arrayString);
